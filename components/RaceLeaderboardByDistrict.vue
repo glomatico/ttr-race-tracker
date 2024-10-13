7bf4ba6d04b2d9ab6cd9fca8a9341466a006e149
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2024-10-13 17:17:18 -0300

    Create RaceLeaderboardByDistrict.vue

---
+++ components/RaceLeaderboardByDistrict.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-data-table-virtual :headers="[{
+        title: 'Toon name',
+        key: 'toonName',
+    },
+    {
+        title: 'Race time',
+        key: 'raceTime',
+    }
+    ]" height="576" :items="raceLeaderboardByDistrictItems" v-show="raceLeaderboardByDistrictShow">
+        <template #item="{ item }">
+            <tr>
+                <td>
+                    <v-avatar>
+                        <v-img
+                            :src="`https://rendition.toontownrewritten.com/render/${item.toonDna}/portrait/128x128.webp`"></v-img>
+                    </v-avatar>
+                    {{ item.toonName }}
+                </td>
+                <td>{{ getFormattedRaceTime(item.raceTime) }}</td>
+            </tr>
+        </template>
+    </v-data-table-virtual>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+    raceLeaderboardByDistrictItems: any[],
+    raceLeaderboardByDistrictShow: boolean,
+}>()
+const { raceLeaderboardByDistrictItems, raceLeaderboardByDistrictShow } = toRefs(props)
+</script>
+
+<style></style>
