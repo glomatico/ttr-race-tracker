98a4d15b68245676107ee5acdbb7cfede6ff5001
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2024-10-13 20:51:44 -0300

    Create default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app>
+        <v-app-bar :title="`Toontown Rewritten Race Tracker`" :color="`#1a5493`">
+        </v-app-bar>
+        <v-main>
+            <slot />
+        </v-main>
+        <v-footer :color="`#1a5493`">
+            <v-container>
+                <v-row>
+                    <p>Developed by Glomatico</p>
+                    <v-spacer></v-spacer>
+                    <v-btn :href="`https://github.com/glomatico/ttr-race-tracker`" :target="`_blank`"
+                        :icon="`mdi-github`" class="mx-4" size="medium" variant="plain"></v-btn>
+                </v-row>
+            </v-container>
+        </v-footer>
+    </v-app>
+
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
