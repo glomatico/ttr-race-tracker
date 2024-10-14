e564f80aa122678a1f6d5f60d173ca7b54d7e103
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2024-10-14 09:53:18 -0300

    added loading

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,27 @@
 <template>
     <v-container>
         <v-row class="ga-4 my-4">
-            <v-select class="w-25" :label="`District`" :items="districtItems" v-model:model-value='selectedDistrict'
-                @update:model-value="onDistrictChange()">
+            <v-select class="w-25" :label="`District`" :disabled="districtsDisabled" :items="districtItems"
+                v-model:model-value='selectedDistrict' @update:model-value="onDistrictChange()">
             </v-select>
             <v-select class="w-25" :label="`Race type`" :disabled="raceTypeDisabled" :items="raceTypeItems"
                 v-model:model-value='selectedRaceType' @update:model-value="onRaceTypeChange()">
             </v-select>
         </v-row>
-        <div v-if="selectedDistrict === 'Global'">
-            <RaceLeaderboardGlobal :items="raceLeaderboardGlobalItems">
-            </RaceLeaderboardGlobal>
+        <div v-if="races === undefined">
+            <div class="text-center">
+                <v-progress-circular indeterminate></v-progress-circular>
+            </div>
         </div>
         <div v-else>
-            <RaceLeaderboardByDistrict :items="raceLeaderboardByDistrictItems">
-            </RaceLeaderboardByDistrict>
+            <div v-if="selectedDistrict === 'Global'">
+                <RaceLeaderboardGlobal :items="raceLeaderboardGlobalItems">
+                </RaceLeaderboardGlobal>
+            </div>
+            <div v-else>
+                <RaceLeaderboardByDistrict :items="raceLeaderboardByDistrictItems">
+                </RaceLeaderboardByDistrict>
+            </div>
         </div>
     </v-container>
 </template>
@@ -29,6 +36,7 @@ useSeoMeta({
 })
 
 const districtItems = ref<string[]>(['Global'])
+const districtsDisabled = ref<boolean>(true)
 const raceTypeItems = ref<string[]>([
     'Airborne Acres',
     'Airborne Acres Rev',
@@ -48,16 +56,16 @@ const raceLeaderboardByDistrictItems = ref<any[]>([])
 const raceLeaderboardGlobalItems = ref<any[]>([])
 const selectedDistrict = ref<string>('')
 const selectedRaceType = ref<string>('')
-
-let races: any = undefined
+const races = ref<any | undefined>(undefined)
 
 async function setRaces() {
     const response = await fetch('https://toontownrewritten.com/api/racing')
-    races = await response.json()
+    races.value = await response.json()
+    districtsDisabled.value = false
 }
 
 async function setDistrictItems() {
-    districtItems.value.push(...Object.keys(races.all_time).sort())
+    districtItems.value.push(...Object.keys(races.value.all_time).sort())
 }
 
 async function onDistrictChange() {
@@ -79,7 +87,7 @@ async function setLeaderboard() {
 }
 
 async function setLeaderboardByDistrictItems() {
-    raceLeaderboardByDistrictItems.value = (races.all_time[selectedDistrict.value][selectedRaceType.value] || []).map((element: any[]) => ({
+    raceLeaderboardByDistrictItems.value = (races.value.all_time[selectedDistrict.value][selectedRaceType.value] || []).map((element: any[]) => ({
         toonName: element[0],
         toonDna: element[1],
         raceTime: element[2]
@@ -87,10 +95,10 @@ async function setLeaderboardByDistrictItems() {
 }
 
 async function setLeaderboardGlobalItems() {
-    raceLeaderboardGlobalItems.value = Object.keys(races.all_time)
+    raceLeaderboardGlobalItems.value = Object.keys(races.value.all_time)
         .sort()
         .flatMap(district =>
-            (races.all_time[district][selectedRaceType.value] || []).map(
+            (races.value.all_time[district][selectedRaceType.value] || []).map(
                 ([toonName, toonDna, raceTime]: [string, string, number]) => ({
                     toonName,
                     toonDna,
