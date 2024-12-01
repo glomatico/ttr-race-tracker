eb028af9817ac20c45c56727a485f30f7f0fee3f
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2024-12-01 00:33:48 -0300

    Create DistrictLeaderboard.vue

---
+++ components/DistrictLeaderboard.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-data-table-virtual :headers="[{
+        title: 'Toon name',
+        key: 'toonName',
+    },
+    {
+        title: 'Race time',
+        key: 'raceTime',
+    }
+    ]" :sort-by="[{ key: 'raceTime', order: 'asc' }]" :items="items">
+        <template #item="{ item }">
+            <tr>
+                <td>
+                    <v-avatar>
+                        <v-img
+                            :src="`https://rendition.toontownrewritten.com/render/${item.toonDna}/portrait/128x128.webp`"></v-img>
+                    </v-avatar>
+                    {{ item.toonName }}
+                </td>
+                <td>{{ getFormattedRaceTime(item.raceTime) }}</td>
+            </tr>
+        </template>
+    </v-data-table-virtual>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        items: {
+            type: Array,
+            required: true
+        }
+    },
+}
+</script>
